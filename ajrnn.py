d293966df916c3780195cd1e15a93404964d327d
Kadam-Tushar <tushar.kadam@somaiya.edu>
2021-11-16 18:43:51 +0530

    basic ajrrn.py added

---
+++ ajrnn.py
@@ -0,0 +1,20 @@
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--batch_size',type=int,required=True)
+    parser.add_argument('--epoch',type=int,required=True)
+    parser.add_argument('--lamda_D',type=float,required=True,help='coefficient that adjusts gradients propagated from discriminator')
+    parser.add_argument('--G_epoch',type=int,required=True,help='frequency of updating AJRNN in an adversarial training epoch')
+    parser.add_argument('--train_data_filename',type=str,required=True)
+    parser.add_argument('--test_data_filename',type=str,required=True)
+
+    parser.add_argument('--layer_num',type=int,required=False,default=1,help='number of layers of AJRNN')
+    parser.add_argument('--hidden_size',type=int,required=False,default=100,help='number of hidden units of AJRNN')
+    parser.add_argument('--learning_rate',type=float,required=False,default=1e-3)
+    parser.add_argument('--cell_type',type=str,required=False,default='GRU',help='should be "GRU" or "LSTM" ')
+    parser.add_argument('--lamda',type=float,required=False,default=1,help='coefficient that balances the prediction loss')
+    parser.add_argument('--D_epoch',type=int,required=False,default=1,help='frequency of updating dicriminator in an adversarial training epoch')
+    parser.add_argument('--GPU',type=str,required=False,default='0',help='GPU to use')
+
+    config = parser.parse_args()
+    main(config)
